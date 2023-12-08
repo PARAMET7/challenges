2e4aa9baf6d5a43f4b4f854dec5e1134cb6a2642
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2023-12-08 16:09:12 +0100

    booleans_booleans  done

---
+++ Booleans/js-conditions-and-booleans_booleans/js/index.js
@@ -0,0 +1,21 @@
+console.clear();
+
+
+
+console.log(Boolean("hello world"));
+console.log(Boolean(2 === 2));
+console.log(Boolean("Try it out"));
+console.log(Boolean("1>10"));
+console.log(Boolean("false"));
+console.log(Boolean(5 > 1));
+console.log(Boolean(50));
+console.log(Boolean(-50));
+console.log(Boolean(true));
+console.log(Boolean("Try it out"));
+
+console.log(Boolean(2 =="2"));
+
+console.log(Boolean(0));
+
+console.log(Boolean(2 === "2"));
+console.log(Boolean(""));
