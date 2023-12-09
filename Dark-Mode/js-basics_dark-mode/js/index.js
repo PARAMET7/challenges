c89f3052ad5b1d3fdea0bb50424ae5fa9cb4d7de
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2023-12-09 13:47:09 +0100

    done:

--- Dark-Mode/js-basics_dark-mode/js/index.js
+++ Dark-Mode/js-basics_dark-mode/js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkButtom = document.querySelector('[data-js="dark-mode-button"]');
+const lightButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+darkButtom.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+  console.log("Darkmode selected");
+});
+
+lightButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+  console.log("lightMode selected");
+});
+
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
