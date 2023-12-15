22442635cb4331aa717c969dad42a1f4703bb67a
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2023-12-15 11:09:42 +0100

    js-createelement_toast-messages

--- JS-createElement/js-createelement_toast-messages/js/index.js
+++ JS-createElement/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,18 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (e) => {
+  e.preventDefault;
+  const t =document.createElement('il');
+  t.classList.add('toast-container__message');
+  t.textContent = 'got a new toast';
+  toastContainer.appendChild(t)
   // Exercise: Append a new entry to the toast messages container
 });
 
-clearButton.addEventListener("click", () => {
+clearButton.addEventListener("click", (e) => {
+  e.preventDefault;
+  toastContainer.innerHTML = ''
+
   // Exercise: Clear the stack of toast messages
 });
