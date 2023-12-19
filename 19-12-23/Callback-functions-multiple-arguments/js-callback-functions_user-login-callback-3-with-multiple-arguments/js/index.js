1a6c3a891111ca3636ba1a7510f045dcb444c5f0
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2023-12-19 16:32:43 +0100

    Callback-functions-multiple-arguments done

--- 19-12-23/Callback-functions-multiple-arguments/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
+++ 19-12-23/Callback-functions-multiple-arguments/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
@@ -5,3 +5,35 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+
+
+
+// In this challenge, you will encounter the following tasks:
+
+// - using multiple parameters inside callback functions
+
+// ## Tasks
+
+// You should write a callback function in two different ways.
+// In both cases your callback function should accept two parameters: "userName" and "userRole".
+// The following message should be logged to the console:
+// "Welcome ${userName}! You are logged in now as ${userRole}."
+
+// 1.  Write a function with the "function" keyword named "showWelcomeMessage".
+//     Call "handleUserLogin" and pass your callback function as the first argument.
+//     (You should see the log from your callback message)
+
+// 2.  Call "handleUserLogin" again but this time write the callback function as an anonymous function **inside** the function call (i.e. between the round brackets).
+//     (You should see the log from your callback message)
+
+// Please switch to the `./js/index.js` file to start the challenge.
+
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin((userName, userRole) => {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+});
