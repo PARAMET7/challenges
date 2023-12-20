8835cae164a99b155eb5db7c4fc9faf31bb516d9
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2023-12-20 11:54:59 +0100

    js-array-methods_map done

--- 20-12-23/map/js-array-methods_map/index.js
+++ 20-12-23/map/js-array-methods_map/index.js
@@ -24,10 +24,40 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((e) =>{
+  return e.answer.toLocaleLowerCase();
+})
+// console.log(lowerCaseAnswers)
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+const questionsAndAnswersTogether = cards.map ((e) => {
+  return e.question + " - " + e.answer;
+
+});
+// console.log(questionsAndAnswersTogether)
+
+
+// ["How often can I use <header>? - As often as you like.", ...]
+
+const questionAndAnswer = cards.map ((e) => {
+  return `question:  ${e.question} , answer: ${e.answer}`
+
+})
+
+console.log(questionAndAnswer)
+
+
+// [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+
+
+
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
+
+
+// In the `./index.js` file, there is a `cards` array with three objects.
+// Below this array, you will find a couple of variables (e.g. `lowerCaseAnswers`). They all are initialized with `null`.
+// And now it's your turn: delete the value `null` and write code such that the variables have a value corresponding to their name. Use `map` to achieve this goal.
+// You will find some hints as comments.
+// Switch to the `./index.js` file and map / filtersome recipes!
+// - If you want to check whether you solved the tasks correctly, run the test following the steps described on "Local Development".
