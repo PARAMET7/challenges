21ace76a78312a26cd6ee59fe621fb4b14a9fa8f
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-01-04 13:31:01 +0100

    witch -c js-structure_shapes

--- 04-01-24/Shapes/js-structure_shapes/index.js
+++ 04-01-24/Shapes/js-structure_shapes/index.js
@@ -1,23 +1,35 @@
 console.clear();
 
+import { getRandomColor } from "./utils/randomColor.js";
+import Circle from "../components/Circle/Circle.js";
+import Square from "../components/Square/Square.js";
+import Pentagon from "../components/Pentagon/Pentagon.js";
 const root = document.getElementById("root");
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
-});
+const circleElement = Circle();
+const squareElement = Square();
+const pentagonElement = Pentagon();
 
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
-});
+root.append(circleElement, squareElement, pentagonElement);
 
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
-});
 
-root.append(circle, square, pentagon);
+
+// const circle = document.createElement("div");
+// circle.classList.add("circle");
+// circle.addEventListener("click", () => {
+//   circle.style.backgroundColor = "#ccc";
+// });
+
+// const square = document.createElement("div");
+// square.classList.add("square");
+// square.addEventListener("click", () => {
+//   square.style.backgroundColor = "#ccc";
+// });
+
+// const pentagon = document.createElement("div");
+// pentagon.classList.add("pentagon");
+// pentagon.addEventListener("click", () => {
+//   pentagon.style.backgroundColor = "#ccc";
+// });
+
+// root.append(circle, square, pentagon);
