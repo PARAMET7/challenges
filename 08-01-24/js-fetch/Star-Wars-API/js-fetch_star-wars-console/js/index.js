99de3c397a0d823338d6ac5dea1d1c711f6b43af
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-01-08 18:15:34 +0100

    Star-Wars-API done:

--- 08-01-24/js-fetch/Star-Wars-API/js-fetch_star-wars-console/js/index.js
+++ 08-01-24/js-fetch/Star-Wars-API/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,41 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    console.log("the response: ", response);
+
+    const data = await response.json();
+    console.log("data: ", data);
+
+
+    const results = data.results;
+    console.log("results: ", results);
+
+
+    console.log(results[2].eye_color);
+  } catch {
+    console.log("error");
+  }
+}
 
 fetchData();
+
+// async function fetchSomething() {
+//   try {
+//     const response = await fetch("/bad/url/oops");
+
+//     if (response.ok) {
+//       // Success (Good Response)
+//       const data = await response.json();
+//       return data;
+//     } else {
+//       // Failure (Bad Response)
+//       console.error("Bad Response");
+//     }
+//   } catch (error) {
+//     // Failure (Network error, etc)
+//     console.error("An Error occurred");
+//   }
+// }
