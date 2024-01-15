8ae6989f65b56b16bc1d5138cbae79c11d80e1d9
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-01-15 17:18:32 +0100

    Buttons done

--- 15-01-24/B/Buttons/react-props_button/src/App.js
+++ 15-01-24/B/Buttons/react-props_button/src/App.js
@@ -2,5 +2,25 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    return console.log("buttom is:");
+  }
+  return (
+    <Button
+      color={"red"}
+      disabled={false}
+      text={"button"}
+      onClick={handleClick}
+    />
+  );
+}
+
+function Button(props) {
+  const {color, disabled, text, onClick } = props // onHandleClick => convention to creat buttom
+  return (<button type="button" style={{ backgroundColor: color }}
+  disabled={disabled}
+  onClick={onClick()}>
+   {text}
+  </button>
+  )
 }
