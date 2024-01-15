67966b5ea428a2b057c020b4438d5c3ba2213271
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-01-15 12:55:48 +0100

    react-basics_attributes done

--- 15-01-24/Attributes/react-basics_attributes/src/App.js
+++ 15-01-24/Attributes/react-basics_attributes/src/App.js
@@ -1,6 +1,22 @@
 import React from "react";
 import "./styles.css";
 
+
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title"> Hello world!</h2>
+      <label htmlFor="myInput">leave me a messege:</label>
+      <input id="htmlFor"></input>
+      <a className="article__link" href="https://eshedyuval.wixsite.com/eshedyuval1">
+        This is what I do
+      </a>
+    </article>
+  );
+
+}
+
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return  <Article />;
 }
