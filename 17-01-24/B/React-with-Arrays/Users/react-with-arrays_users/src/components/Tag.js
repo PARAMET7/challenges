a182e0594eae719b29a83918731311b4b23168e1
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-01-17 17:15:38 +0100

    react-with-arrays_users

--- 17-01-24/B/React-with-Arrays/Users/react-with-arrays_users/src/components/Tag.js
+++ 17-01-24/B/React-with-Arrays/Users/react-with-arrays_users/src/components/Tag.js
@@ -1,5 +1,7 @@
 import "./Tag.css";
 
-export default function Tag({ tag }) {
-  return <li className="tag">{tag}</li>;
+export default function Tag({ tag, adminClass  }) {
+  // return <li className="tag">{tag}</li>;
+
+    return <li className={`tag ${adminClass}`}>{tag}</li>;
 }
