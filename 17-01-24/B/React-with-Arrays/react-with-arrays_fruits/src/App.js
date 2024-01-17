9391ed7b0490e82b1a0ff4682d26e211effcdcf2
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-01-17 16:06:56 +0100

    react-with-arrays_fruits

--- 17-01-24/B/React-with-Arrays/react-with-arrays_fruits/src/App.js
+++ 17-01-24/B/React-with-Arrays/react-with-arrays_fruits/src/App.js
@@ -1,11 +1,40 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 2,
+      name: "🍊 Orange",
+      color: "Orange",
+    },
+    {
+      id: 3,
+      name: "🍍 pineappel",
+      color: "yellow",
+    },
+    {
+      id: 4,
+      name: "🍒 cherry",
+      color: "red",
+    },
+    {
+      id: 5,
+      name: "🥝 kivi",
+      color: "green",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {/* <Card name="🍌 banana" /> */}
+      {fruits.map((e) => (
+        <Card color={e.color} key={e.id} name={e.name} />
+      ))}
     </div>
   );
 }
--- 17-01-24/B/React-with-Arrays/react-with-arrays_fruits/src/components/Card.js
+++ 17-01-24/B/React-with-Arrays/react-with-arrays_fruits/src/components/Card.js
@@ -1,5 +1,10 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ name, color }) {
+  // return <p className="card">{name}</p>;
+  return (
+    <p className="card" style={{ backgroundColor: `${color}` }}>
+      {name}
+    </p>
+  );
 }
