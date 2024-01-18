6d8f8cd548847c8c0199ba8f03e3c23dc3496843
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-01-18 11:43:25 +0100

    Table-Reservation

--- 18-01-24/A/Table-Reservation/react-state-2_table-reservation/src/App.js
+++ 18-01-24/A/Table-Reservation/react-state-2_table-reservation/src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  //Move the `people` state to the `App` component.
+   const [people, setPeople] = useState(0);
+
+  //Pass your `handle`:
+  function Increment() {
+    setPeople(people + 1);
+  }
+
+  function Decrement() {
+    setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        People={people}
+        Increment={Increment}
+        Decrement={Decrement}
+      />
+      <p>You are going to reserve a table for: {people}  people.</p>
     </div>
   );
 }
