942134bc37ee4742ecfff1153a50e628192f6d59
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-01-19 11:37:35 +0100

    react-state-3_adding-animals done

--- 19-01-24/A/Adding-Animals/react-state-3_adding-animals/src/App.js
+++ 19-01-24/A/Adding-Animals/react-state-3_adding-animals/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./styles.css";
 import Form from "./components/Form/index.js";
 import List from "./components/List/index.js";
+import { uid } from "uid";
 
 const initialAnimals = [
   {
@@ -25,8 +26,11 @@ export default function App() {
   const [animals, setAnimals] = useState(initialAnimals);
 
   function handleAddAnimal(newAnimal) {
-    console.log(newAnimal);
+    // console.log("newAnimal:" ,newAnimal);
+    setAnimals([...animals, { ...newAnimal, id: uid() }]);
+
   }
+
 
   return (
     <main className="app">
