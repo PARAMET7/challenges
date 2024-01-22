600d79019b695679c134b6592e65e913f390d66e
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-01-22 12:59:45 +0100

    react-with-local-storage_dice done

--- 22-01-24/A/Dice/react-with-local-storage_dice/src/App.js
+++ 22-01-24/A/Dice/react-with-local-storage_dice/src/App.js
@@ -1,15 +1,25 @@
 import { D6Button } from "./components/D6Button";
 import History from "./components/History";
 import "./styles.css";
+import useLocalStorageState from "use-local-storage-state";
+import { getD6Roll } from "./utils";
 
 export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("", { defaultValue: [] });
+  console.log(rolls);
+
+  const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  };
+
+
   return (
     <div className="app">
       <main className="app__main">
-        <D6Button />
+        <D6Button value={rolls} onRoll={handleRoll}  />
       </main>
       <aside className="app__aside">
-        <History rolls={[]} />
+        <History  rolls={rolls} />
       </aside>
     </div>
   );
--- 22-01-24/A/Dice/react-with-local-storage_dice/src/components/D6Button/index.js
+++ 22-01-24/A/Dice/react-with-local-storage_dice/src/components/D6Button/index.js
@@ -1,20 +1,23 @@
-import { useState } from "react";
-import { getD6Roll } from "../../utils";
+// import { useState } from "react";
+// import { getD6Roll } from "../../utils";
 import D6 from "../D6";
 import "./D6Button.css";
 
-export function D6Button() {
-  const [rolls, setRolls] = useState([]);
+// export function D6Button() {
+//   const [rolls, setRolls] = useState([]);
 
-  const handleRoll = () => {
-    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
-  };
+//   const handleRoll = () => {
+//     setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+//   };
+// }
 
-  const currentRollValue = rolls[0]?.value;
+export function D6Button({ value, onRoll }) {
+  const rollValue = value[0]?.value;//???
+  console.log("rollValue=>",rollValue);
 
   return (
-    <button className="d6-button" type="button" onClick={handleRoll}>
-      <D6 value={currentRollValue} />
+    <button className="d6-button" type="button" onClick={onRoll}>
+      <D6 value={rollValue} />
     </button>
   );
 }
