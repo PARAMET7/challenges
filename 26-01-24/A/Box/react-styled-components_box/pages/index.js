3cd4c4d1d4a04981ddd685052cf61e2330bae0a0
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-01-26 12:25:05 +0100

    react-styled-components_box done

--- 26-01-24/A/Box/react-styled-components_box/pages/index.js
+++ 26-01-24/A/Box/react-styled-components_box/pages/index.js
@@ -1,10 +1,18 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "/Users/eyl/Desktop/spiced-bootcamp/challenges1/26-01-24/A/Box/react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js";
+
+import StyleContaine from "../components/BoxWithClassName/StyleContaine.js";
 
 export default function HomePage() {
   return (
-    <div>
-      <BoxWithClassName />
-      <BoxWithClassName isBlack />
+    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+      <StyleContaine>
+        <BoxWithClassName />
+        <BoxWithClassName isBlack />
+
+        <BoxWithStyledComponents />
+        <BoxWithStyledComponents $isBlack="isBlack" />
+      </StyleContaine>
     </div>
   );
 }
